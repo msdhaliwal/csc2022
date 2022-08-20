a7e253839d675fc465ddcb8e8400db038d0a9c51
Manpreet Dhaliwal <msdhaliwal@outlook.in>
2022-08-20 19:59:47 +0530

    custom UI

---
+++ public/script.js
@@ -0,0 +1,83 @@
+const tBody = document.querySelector('#tBody');
+const searchBox = document.querySelector('#searchBox');
+
+const search = (arr, searchKey) => arr.filter((obj) => Object.keys(obj).some((key) => String(obj[key].toLowerCase()).includes(String(searchKey).toLowerCase())));
+// const search = objects.filter(item => `${item.foo} ${item.bar}`.includes(query));
+
+const GetWaitingTime = (cb) => {
+	try {
+		const xhr = new XMLHttpRequest();
+		xhr.onreadystatechange = function () {
+			if (this.readyState == 4 && this.status == 200) {
+				const response = JSON.parse(xhr.response);
+				cb(null, response);
+			}
+		};
+		xhr.open('GET', `/api/response-time`, 'true');
+		xhr.setRequestHeader('content-type', 'application/json');
+		xhr.send();
+	} catch (error) {
+		cb(error, null);
+	}
+};
+
+const PopulateTable = (data) => {
+	if (!data || data.length <= 0) {
+		tBody.innerHTML = '<h4>No Data Found</h4>';
+		return;
+	}
+	tBody.innerHTML = '';
+	let index = 1;
+	for (const row of data) {
+		const elem = TableRow(row, index);
+		tBody.appendChild(elem);
+		index++;
+	}
+};
+
+const TableRow = ({ value: name, time1, time2, time3, code }, index) => {
+	const rowElem = document.createElement('tr');
+	const srNoElem = document.createElement('td');
+	const nameElem = document.createElement('td');
+	const time1Elem = document.createElement('td');
+	const time2Elem = document.createElement('td');
+	const time3Elem = document.createElement('td');
+
+	rowElem.id = code;
+
+	srNoElem.textContent = index;
+	nameElem.textContent = name;
+	time1Elem.textContent = time1;
+	time2Elem.textContent = time2;
+	time3Elem.textContent = time3;
+
+	rowElem.appendChild(srNoElem);
+	rowElem.appendChild(nameElem);
+	rowElem.appendChild(time1Elem);
+	rowElem.appendChild(time2Elem);
+	rowElem.appendChild(time3Elem);
+
+	return rowElem;
+};
+// event listeners
+searchBox.addEventListener('input', (event) => {
+	const searchKey = event.target.value.trim();
+	if (!searchKey) PopulateTable(tableData);
+	else {
+		let data = search(tableData, searchKey);
+		PopulateTable(data);
+	}
+});
+//
+let tableData;
+(() => {
+	GetWaitingTime((error, data) => {
+		if (error) {
+			console.log('error');
+		} else {
+			searchBox.readOnly = false;
+			tableData = data;
+			PopulateTable(tableData);
+		}
+	});
+})();
